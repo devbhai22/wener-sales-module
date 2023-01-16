4ff4784b49cb68c1909ee6ba4ef343c23c7b73ba
Muhtasim Hossain <muhtasimhossain43@gmail.com>
2023-01-16 15:31:59 +0600

    updated orders table

---
+++ wener-chairman/src/components/Aggregation.js
@@ -0,0 +1,20 @@
+import { Divider } from '@mui/material'
+import { Stack } from '@mui/system'
+import React from 'react'
+
+const Aggregation = ({array, keys}) => {
+  return (
+    <Stack direction="row" 
+        divider={<Divider orientation="vertical" flexItem />}
+        spacing={2}
+        sx={{marginLeft:"15px", marginTop:"-40px"}}
+    >
+        <span>Items: {array.length.toLocaleString()}</span>
+        {keys.map((key)=>
+            <span>{key.label}: {array.reduce((sum, el)=> sum + parseFloat(!el[key.key]?0:el[key.key]), 0).toLocaleString()}</span>
+        )}
+    </Stack>
+  )
+}
+
+export default Aggregation
